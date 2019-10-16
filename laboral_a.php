b92ae81029dece48157d6b79de2b43f3842bc4dc
Real6 <ruben.aguilera8160@gmail.com>
2019-10-16 09:37:29 +0200

    version 31

--- laboral_a.php
+++ laboral_a.php
@@ -1,9 +1,34 @@
 <?php
-function mes($n) {
-	$a=[
-	
-]
+
+
+for($i=1;$i<366;$i++){
+	$r[]=[
+	'n'=>$i,
+	's'=>round($i/7)+1,
+	'fecha'=>date('Y-m-d',strtotime('2019-01-01')+($i-1)*24*60*60),
+	'D'=>date('D',strtotime('2019-01-01')+($i-1)*24*60*60),
+	];
 }
 
-echo mes(1)."<br>";
+foreach($r as $f){
+	echo '<tr>'."<br>";
+	
+	echo '<td>'."<br>";
+	echo $f['n'];
+	echo '</td>';	
+
+	echo '<td>'."<br>";
+	echo $f['s'];
+	echo '</td>';
+	
+	echo '<td>'."<br>";
+	echo $f['fecha'];
+	echo '</td>';	
+
+	echo '<td>'."<br>";
+	echo $f['D'];
+	echo '</td>';	
+	
+	echo '</tr>';
+}
 ?>
