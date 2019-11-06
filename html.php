f1e4ed650bca3361e745431ccc6bde6069215da2
Real6 <ruben.aguilera8160@gmail.com>
2019-11-06 09:12:13 +0100

    version 38

--- html.php
+++ html.php
@@ -3,11 +3,23 @@
 <head>
 
 <title> Tema 3 </title>
+<h1> Vocales </h1>
 
 </head>
 <body>
 
-<h1> Actividad 1 </h1>
+<h2> AWA </h2> 
+
+ <b> <font size=20> EWE </font> </b> </br>
+ 
+ </br> <u> <font color="blue"> IWI </font> </u> 
+ 
+ <h3> OWO </h3>
+ 
+ <h3>  <center> UWU </center>  </h3>
+
+ <img src="https://i.ytimg.com/vi/MooO02VdfVU/maxresdefault.jpg" width="600">
+
 
 </body>
 
